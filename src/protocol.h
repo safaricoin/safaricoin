af81d4174160fb19bbe5f5b3e8b6fa215580e220
Intellect Alliance <safaricoin@funtrench.com>
2013-11-21 09:38:42 +0300

    Safaricoin Point Echo-RC

--- src/protocol.h
+++ src/protocol.h
@@ -19,7 +19,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 30587 : 20587;
+    return testnet ? 30586 : 20587;
 }
 
 
